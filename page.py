ec15c1e856698d43e92d1a0b4d22a67d4de3be0d
aprameyavaidya1 <aprameyavaidya1@gmail.com>
2025-11-11 12:46:30 +0530

    Create page.py
    Fix age eligibility check and handle exceptions properly.

---
+++ page.py
@@ -0,0 +1,8 @@
+try:
+age=int(input("Enter your age:"))
+if age>=18;
+print("your eligible to vote:")
+else:
+print("your are not eligible to vote")
+except valueError:
+print("please enter vaild integer for age.")
